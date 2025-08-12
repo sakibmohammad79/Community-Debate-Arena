e5a8bff357aa3fc69ea8ab0061cba677cf334b8f
sakib <mohammadsakib7679@gmail.com>
2025-08-12 12:32:06 +0600

    project initialize for community debate arena app

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello Welcome To The Debate Arena!");
+})
+
+export default app;
